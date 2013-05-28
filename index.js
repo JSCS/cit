29298adc5de77160800567a9da5b6e64b2b54679
Julian <Julian.Schwartz@gmail.com>
2013-05-28 12:25:57 -0700

    start

--- index.js
+++ index.js
@@ -21,15 +21,33 @@ $(function(){
 
   function reload_citation(){
     citation = new Citation($('#citations').val());
-    citation.ready(function(){
-      $('#citation_volume').text(citation.volume);
+    citation.ready(function(){      
       $('#citation_citation').text(citation.citation);
-      $('#citation_text').text(citation.text);
-      $('#citation_url').text(citation.url);
+      $('#citation_text').text(citation.text);      
     });
   }
 
   $('#citations').change(reload_citation);
   $('#citations').keyup(reload_citation);
+  $('#citations').keydown(function (e){
+    if(e.keyCode == 13){
+      if ($('#citation_citation').text() != "Keep typing..") {
+        f = $('#cite1').clone();
+	f.hide()
+	$('#cite1').before(f)
+	f.fadeIn();
+	f.attr('id','x')
+	f.children().attr('id','x')
+	$('#citation_citation').text('Keep typing..');
+	$('#citation_text').text("..to add more citations. Try: bible Joshua 3:1-5 or science_health 45:1-12");      	
+	$('#citations').val('science_health 3:');
+      }
+    }          
+  })
+  reload_citation();  
+});
+$( document ).ready(function() {
+  $('#citations').focus();
+  $('#citations').val("bible_kjv Gen. 12:1-3 the (to :)");
   reload_citation();
 });
